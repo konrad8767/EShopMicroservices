98731a3da052d96491cb087e025058917261ed10
Konrad Prusaczyk <tornado1999@op.pl>
2025-04-14 03:10:21 +0200

    Created Ordering.Infrastructure layer with Code First approach using EF Core and SQL Server with initial data seeding.

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Ordering.Application
 {
@@ -6,10 +7,10 @@ namespace Ordering.Application
     {
         public static IServiceCollection AddAplicationServices(this IServiceCollection services)
         {
-            //services.AddMediatR(cfg =>
-            //{
-            //    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-            //});
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
 
             return services;
         }
