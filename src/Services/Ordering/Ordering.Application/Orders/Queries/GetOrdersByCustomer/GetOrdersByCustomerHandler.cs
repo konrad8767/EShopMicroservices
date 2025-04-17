ac73036824028b2f8a94ce86e36d82e31004ea2a
Konrad Prusaczyk <tornado1999@op.pl>
2025-04-17 02:01:29 +0200

    Created CQRS Queries like GetOrdersByCustomer, GetOrdersByName and GetOrders with Pagination.

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,21 @@
+﻿
+using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer
+{
+    public class GetOrdersByCustomerHandler(IApplicationDbContext dbContext)
+        : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+    {
+        public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+        {
+            var orders = await dbContext.Orders
+                            .Include(x => x.OrderItems)
+                            .AsNoTracking()
+                            .Where(x => x.CustomerId == CustomerId.Of(query.CustomerId))
+                            .OrderBy(x => x.OrderName.Value)
+                            .ToListAsync(cancellationToken);
+
+            return new GetOrdersByCustomerResult(orders.ToOrderDtoList());
+        }
+    }
+}
